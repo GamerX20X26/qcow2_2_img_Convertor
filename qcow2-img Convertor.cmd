ce545dcef6325509e2dc217719e086ca04dbabd4
GamerX20X26 <igamerx20x@tutamail.com>
2026-07-21 08:24:23 -0500

    Update qcow2-img Convertor.cmd

--- qcow2-img Convertor.cmd
+++ qcow2-img Convertor.cmd
@@ -1,4 +1,5 @@
 @echo off
+setlocal enabledelayedexpansion
 cls
 :MainMenu
 echo ===================================================
@@ -42,30 +43,36 @@ if errorlevel 1 goto :MakeImg
 
 :MakeImg
 echo.
-set /p img_name="Enter image filename (e.g., win7.img): "
-set /p img_size="Enter size (e.g., 20G, 100G, 1T): "
+set /p "img_name=Enter image filename/path (e.g., win7.img): "
+set /p "img_size=Enter size (e.g., 20G, 100G, 1T): "
 
-:: Bug fix: Automatically append .img if missing
-if not "%img_name:~-4%"==".img" set "img_name=%img_name%.img"
+:: Remove any existing quotes entered by user
+set "img_name=%img_name:"=%"
+
+:: Automatically append extension if missing
+if /i not "!img_name:~-4!"==".img" set "img_name=!img_name!.img"
 
 echo.
 echo Processing...
-qemu-img create -f raw "%img_name%" %img_size%
+qemu-img create -f raw "!img_name!" %img_size%
 echo.
 pause
 goto :MainMenu
 
 :MakeQcow2
 echo.
-set /p qcow_name="Enter image filename (e.g., win7.qcow2): "
-set /p qcow_size="Enter size (e.g., 20G, 100G, 1T): "
+set /p "qcow_name=Enter image filename/path (e.g., win7.qcow2): "
+set /p "qcow_size=Enter size (e.g., 20G, 100G, 1T): "
 
-:: Bug fix: Automatically append .qcow2 if missing
-if not "%qcow_name:~-6%"==".qcow2" set "qcow_name=%qcow_name%.qcow2"
+:: Remove any existing quotes entered by user
+set "qcow_name=%qcow_name:"=%"
+
+:: Automatically append extension if missing
+if /i not "!qcow_name:~-6!"==".qcow2" set "qcow_name=!qcow_name!.qcow2"
 
 echo.
 echo Processing...
-qemu-img create -f qcow2 "%qcow_name%" %qcow_size%
+qemu-img create -f qcow2 "!qcow_name!" %qcow_size%
 echo.
 pause
 goto :MainMenu
@@ -76,15 +83,20 @@ echo ===================================================
 echo             CONVERT QCOW2 TO RAW IMG
 echo ===================================================
 echo.
-set /p src_qcow="Enter source QCOW2 filename: "
-set /p dest_img="Enter destination IMG filename: "
+set /p "src_qcow=Enter source QCOW2 filename/path: "
+set /p "dest_img=Enter destination IMG filename/path: "
 
-if not "%src_qcow:~-6%"==".qcow2" set "src_qcow=%src_qcow%.qcow2"
-if not "%dest_img:~-4%"==".img" set "dest_img=%dest_img%.img"
+:: Remove any existing quotes entered by user
+set "src_qcow=%src_qcow:"=%"
+set "dest_img=%dest_img:"=%"
+
+:: Automatically append extensions if missing
+if /i not "!src_qcow:~-6!"==".qcow2" set "src_qcow=!src_qcow!.qcow2"
+if /i not "!dest_img:~-4!"==".img" set "dest_img=!dest_img!.img"
 
 echo.
 echo Processing conversion...
-qemu-img convert -f qcow2 -O raw "%src_qcow%" "%dest_img%"
+qemu-img convert -f qcow2 -O raw "!src_qcow!" "!dest_img!"
 echo.
 pause
 goto :MainMenu
@@ -95,15 +107,20 @@ echo ===================================================
 echo          CONVERT IMG TO COMPRESSED QCOW2
 echo ===================================================
 echo.
-set /p src_img="Enter source IMG filename: "
-set /p dest_qcow="Enter destination QCOW2 filename: "
+set /p "src_img=Enter source IMG filename/path: "
+set /p "dest_qcow=Enter destination QCOW2 filename/path: "
 
-if not "%src_img:~-4%"==".img" set "src_img=%src_img%.img"
-if not "%dest_qcow:~-6%"==".qcow2" set "dest_qcow=%dest_qcow%.qcow2"
+:: Remove any existing quotes entered by user
+set "src_img=%src_img:"=%"
+set "dest_qcow=%dest_qcow:"=%"
+
+:: Automatically append extensions if missing
+if /i not "!src_img:~-4!"==".img" set "src_img=!src_img!.img"
+if /i not "!dest_qcow:~-6!"==".qcow2" set "dest_qcow=!dest_qcow!.qcow2"
 
 echo.
 echo Processing compression and conversion...
-qemu-img convert -f raw -O qcow2 -c "%src_img%" "%dest_qcow%"
+qemu-img convert -f raw -O qcow2 -c "!src_img!" "!dest_qcow!"
 echo.
 pause
 goto :MainMenu
